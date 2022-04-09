2bb47ac9f19403bed627f21fa7511bb719f422a5
Kamran Ahmadoff <101476174+k0mrn@users.noreply.github.com>
2022-04-10 01:31:14 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ekenmeneatadesin
+İnsan adları tapmağ üçün @K0mran tərəfindən yaradılmış oyun
---
+++ settings.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+TOKEN = "5113635118:AAE0jr2U4rV33fA-hi_PonIk_9dXB_G5KhU"
+with open('words.txt', 'r', encoding='utf-8') as file:
+    word_list = file.read().splitlines()
